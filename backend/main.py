81e5143f819de9dc4b289037aeade13f8eaf627d
Nikita Kelwada <nikita.kelwada.01@gmail.com>
2024-11-09 17:54:04 -0600

    Changes to Dev Setup

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,36 @@
 from flask import Flask, request
 from dotenv import load_dotenv
 import os
+from openai import OpenAI
 
+#TASK:
+#make a route, use Open AI API, create API key and hide in .env file, use gitignore
+#Function 1: Use API, make a call, take in a 1D array, method passes to the prompt (use prompt engr), API call function 
+#Function 2: parse the response, get the grouping, make and return into an array
+#make and add to git ignore, copy paste API key, make request
 
+#GIT COMMANDS (uwu)
+#git add . 
+#git status
+#git commit -m "name"
+#git push
+#git status
+#git log
+#git pull
+#git checkout -b "name" (create branch)
+
+#load env variables
+load_dotenv()
+
+#get key + check
+api_key = os.getenv('api_key')
+print(f"API Key Loaded: {api_key}")
+
+#initialize clinet + flask
+client = OpenAI(api_key)
 app = Flask(__name__)
 
+#ALL ROUTES
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
@@ -19,25 +45,22 @@ def test_input():
     print(name)
     return f'<p>{name}<p>'
 
-#make route, use opanAI API for python, ask Nafi for API key, create .env file, 
-#make and add to git ignore, copy paste API key, make request, 
-# return ans yas, 
-# function with open AI API, make call, method passes to the prompt w/ prompt engr, API clal function
-# parse the response, get the grouping, make into array 
+#OPEN AI API
+@app.route("/get-response", methods=["GET"])
+def get_response():
+    #take user input from 'input'
+    user_input = request.args.get('input', '')
 
-#make secrets.txt, add .gitignore, then import api key from secrests make it a variable in code.
-#with read, with open " " file, parse line, get API key, save as variable
-load_dotenv()
-api_key = os.getenv('api_key')
-from openai import OpenAI
-client = OpenAI(os.getemv(api_key))
+    #make API call
+    response = client.chat.completions.create(
+        messages=[{
+            "role": "user",
+            "content": "Say this is a test",
+        }],
+        model="gpt-4o-mini",
+    )
+    #print request ID (do i need this?)
+    print(response._request_id)
 
-response = client.chat.completions.create(
-    messages=[{
-        "role": "user",
-        "content": "Say this is a test",
-    }],
-    model="gpt-4o-mini",
-)
-
-print(response._request_id)
+    #returns response
+    return f"<p>{response['choices'][0]['message']['content']}</p>"
